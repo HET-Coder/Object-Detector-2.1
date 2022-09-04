3bb9d15cae56cef4d156d84f43886d6e7633598c
HET-Coder <95082442+HET-Coder@users.noreply.github.com>
2022-09-04 18:23:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function preload() {
+    img = loadImage('Living.jpg');
+}
+
+function setup() {
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
